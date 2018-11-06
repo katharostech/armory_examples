795910155fd663e7f7a72639bac533aecce6b656
Zicklag <zicklag@katharostech.com>
2018-11-06 14:15:32 -0600

    Updated Modding Example Instruction Text

--- modding/Mods/mod1/Sources/mod1/node/MoveCube.hx
+++ modding/Mods/mod1/Sources/mod1/node/MoveCube.hx
@@ -2,7 +2,16 @@ package mod1.node;
 
 @:keep class MoveCube extends armory.logicnode.LogicTree {
 
-	public function new() { super(); notifyOnAdd(add); }
+	var functionNodes:Map<String, armory.logicnode.FunctionNode>;
+
+	var functionOutputNodes:Map<String, armory.logicnode.FunctionOutputNode>;
+
+	public function new() {
+		super();
+		this.functionNodes = new Map();
+		this.functionOutputNodes = new Map();
+		notifyOnAdd(add);
+	}
 
 	override public function add() {
 		var _SetLocation = new armory.logicnode.SetLocationNode(this);
